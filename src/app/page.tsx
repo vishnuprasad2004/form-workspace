fd179dbb6b895848185caf7c53c150194408c38b
Vishnu Prasad Korada <vishnuprasadkorada@gmail.com>
2024-07-10 00:45:51 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import React from "react";
+
+export default function Home() {
+  return (
+    <main className={"flex min-h-screen flex-col items-center justify-between p-24"}> 
+      <h1 className={"text-3xl font-bold"}>Hello World!</h1>
+    </main>
+  );
+}
